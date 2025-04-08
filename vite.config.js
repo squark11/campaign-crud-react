12bbcdbbad37060e50cfc2612c35d9c1b6f8225c
squark11 <kacper.mackowiak@microsoft.wsei.edu.pl>
2025-04-08 21:22:59 +0200

    Update base path in Vite configuration for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/campaign-crud-react/",
+  base: '/campaign-crud/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
