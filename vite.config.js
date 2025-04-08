12d29e6fde8ef51b494af53e830fc0fb7f9a68a2
squark11 <kacper.mackowiak@microsoft.wsei.edu.pl>
2025-04-08 21:35:36 +0200

    sada

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/campaign-crud-react/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
